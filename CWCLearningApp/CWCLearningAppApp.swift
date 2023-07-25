09fa9ab26de96f6e64b14bf5bece372b1a8f31ad
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-24 21:16:00 -0400

    Initial Commit

---
+++ CWCLearningApp/CWCLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWCLearningAppApp.swift
+//  CWCLearningApp
+//
+//  Created by Chuck Belcher on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CWCLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
