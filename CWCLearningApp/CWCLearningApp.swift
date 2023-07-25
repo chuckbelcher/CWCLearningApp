d06858eb4af8e70a88771b47f6e22af81ce3c1e2
Chuck Belcher <cfbelcher@pateogroup.com>
2023-07-24 21:21:54 -0400

    Initial commit created standard set of folders

--- CWCLearningApp/CWCLearningApp.swift
+++ CWCLearningApp/CWCLearningApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct CWCLearningAppApp: App {
+struct CWCLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
